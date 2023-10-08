12d657f0f96a97deb4c00972435a961d9c58d936
narcolin <narcolin@calpoly.edu>
2023-10-08 13:23:52 -0700

    cors

--- backend.js
+++ backend.js
@@ -1,122 +1,119 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
 
 const app = express();
 const port = 8000;
 
 const users = { 
-    users_list :
-    [
-       { 
-          id : 'xyz789',
-          name : 'Charlie',
-          job: 'Janitor',
-       },
-       {
-          id : 'abc123', 
-          name: 'Mac',
-          job: 'Bouncer',
-       },
-       {
-          id : 'ppp222', 
-          name: 'Mac',
-          job: 'Professor',
-       }, 
-       {
-          id: 'yat999', 
-          name: 'Dee',
-          job: 'Aspring actress',
-       },
-       {
-          id: 'zap555', 
-          name: 'Dennis',
-          job: 'Bartender',
-       },
-       {
-        "id": "qwe123",
-        "job": "Zookeeper",
-        "name": "Cindy"
-     }
-    ]
- }
- 
+   users_list : [
+      { 
+         id : 'xyz789',
+         name : 'Charlie',
+         job: 'Janitor',
+      },
+      {
+         id : 'abc123', 
+         name: 'Mac',
+         job: 'Bouncer',
+      },
+      {
+         id : 'ppp222', 
+         name: 'Mac',
+         job: 'Professor',
+      }, 
+      {
+         id: 'yat999', 
+         name: 'Dee',
+         job: 'Aspring actress',
+      },
+      {
+         id: 'zap555', 
+         name: 'Dennis',
+         job: 'Bartender',
+      }
+   ]
+}
+
 app.use(cors());
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// });
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
-// app.get('/users', (req, res) => {
-//     res.send(users);
-// });
+const findUserByName = (name) => { 
+    return users['users_list']
+        .filter( (user) => user['name'] === name); 
+}
+
+const findUserByNameAndJob = (name, job) => {
+    return users['users_list']
+	.filter( (user) => user['name'] === name && user['job'] === job )
+}
 
 app.get('/users', (req, res) => {
     const name = req.query.name;
-    if (name != undefined){
-        let result = findUserByNameandJob(name);
-        result = {users_list: result};
-        res.send(result);
+    const job = req.query.job;
+    if (name != undefined && job != undefined){
+	let result = findUserByNameAndJob(name, job);
+	result = {users_list: result};
+	res.send(result);
     }
-    else if (name != undefined) {
+    else if(name != undefined){
         let result = findUserByName(name);
         result = {users_list: result};
         res.send(result);
     }
-    else {
-        res.send(result);
+    else{
+        res.send(users);
     }
 });
 
+const findUserById = (id) =>
+    users['users_list']
+        .find( (user) => user['id'] === id);
+    
 app.get('/users/:id', (req, res) => {
     const id = req.params['id']; //or req.params.id
-    let result = findUserById(id);
-    if (result === undefined || result.length == 0)
+    let result = findUserById(id); 
+    if (result === undefined) {
         res.status(404).send('Resource not found.');
-    else {
-        result = {users_list: result};
+    } else {
         res.send(result);
     }
 });
 
-function findUserById(id) {
-    return users['users_list'].find( (user) => user['id'] === id); // or line below
-    //return users['users_list'].filter( (user) => user['id'] === id);
-}
+const addUser = (user) => {
+    users['users_list'].push(user);
+    return user;
+}	
 
-const findUserByName = (name) => { 
-    return users['users_list'].filter( (user) => user['name'] === name); 
-}
-
-const findUserByNameandJob = (name, job) => {
-    return users['users_list'].filter((user) => user['name'] === name && user['job'] === job);
+function  genId() {
+    return Math.floor(Math.random() * 1000);
 }
 
 app.post('/users', (req, res) => {
-    const userToAdd = req.body;
-    addUser(userToAdd);
-    res.status(200).end();
+    const userAdd = req.body;
+    userAdd.id = genId().toString();
+    addUser(userAdd);
+    res.status(201).send(JSON.stringify(userAdd));
 });
 
-function addUser(user){
-    users['users_list'].push(user);
-}
+const findUserIndexById = (id) =>
+    users['users_list']
+        .findIndex( (user) => user['id'] === id);
 
 app.delete('/users/:id', (req, res) => {
-    const id = req.params['id'];
-    let result = deleteUser(id);
-    if (result === -1)
+    const id = req.params['id']; //or req.params.id
+    let result = findUserIndexById(id); 
+    if (result < 0) {
         res.status(404).send('Resource not found.');
-    res.status(200);
+    } else {
+	users['users_list'].splice(result, 1);
+        res.status(204).send();
+    }
 });
-
-function deleteUser(id){
-    const indexToDelete = users['users_list'].findIndex((user) => user['id'] === id);
-    if (indexToDelete !== -1)
-        users['users_list'].splice(indexToDelete, 1);
-    return indexToDelete;
-}
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-});
+});  
